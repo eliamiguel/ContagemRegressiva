781a7de50ec9eea6d1c50b558463ff98688c0327
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2024-06-04 22:27:00 -0300

    melhorando o codigo

--- main.js
+++ main.js
@@ -1,70 +1,54 @@
+
 let id;
+const mensagem = document.getElementById('tempoEsgotado')
 
-const formatar= (tempo)=> `0${tempo}`.slice(-2) 
-
-const atualizar = (tempo)=> {
-
+const formatar = (tempo)=> `0${tempo}`.slice(-2)
+const atualizar = (tempo)=>{
   const segundo = document.getElementById('segundos')
-  const minutos = document.getElementById('minutos')
+  const minuto = document.getElementById('minutos')
   const horas = document.getElementById('horas')
   const dias = document.getElementById('dias')
 
-  const qtdSegundo = tempo % 60;
-  const qtdMinutos = parseInt(tempo % (60 * 60) / 60)
-  const qtdHoras = parseInt(tempo % (60 * 60 * 24) / (60 * 60))
-  const qtdDias = parseInt(tempo / (60 * 60 * 24) )
+  const qtdSegundos = tempo % 60
+  const qtdMinutos = Math.floor(tempo % (60 * 60) /60)
+  const qtdHoras = Math.floor(tempo % (60 * 60 * 24) / (60 * 60))
+  const qtdDias = Math.floor(tempo / (60 * 60 * 24))
 
 
-  segundo.innerHTML= formatar(qtdSegundo)
-  minutos.innerHTML= formatar(qtdMinutos)
+
+  segundo.innerHTML= formatar(qtdSegundos)
+  minuto.innerHTML= formatar(qtdMinutos)
   horas.innerHTML= formatar(qtdHoras)
- dias.innerHTML= formatar(qtdDias)
-  
-
+  dias.innerHTML= formatar(qtdDias)
 }
 
-
-
-const pararTempo =()=>{
-  clearInterval(id)
-}
-
-const contagemRegresssiva = (tempo)=>{
+const pararContagem = ()=>clearInterval(id)
+const contagemRegressiva = (tempo)=>{
 
   const contar = ()=>{
-    if(tempo >= 0 ){
-      atualizar(tempo)
-     tempo--
+      if(tempo < 0){
+          pararContagem()
+          mensagem.innerHTML='tempo de inscrição esgotado'
+      }else{
+          atualizar(tempo)
+          tempo--
+    }
       
-    }
-    if (tempo === 0) {
-      pararTempo()
-       const tempoEsgotado = document.getElementById('tempoEsgotado')
-       tempoEsgotado.textContent='O tempo para inscreção para o evento se esgotou'
-       tempoEsgotado.classList.add('tempoEsgotado')
-        document.getElementById('segundos').innerHTML= '00'
-       return;
-
-    }
-   
-   
-   
-  
   }
-   
-  id=setInterval(contar, 1000)
 
 
-
+  id = setInterval(contar, 1000)
 }
 
 
+const tempoRestante = ()=>{
+  const data = new Date('2024-06-04 22:20:00')
+  const hoje = Date.now()
 
-const dataEvento = ()=> {
-const data = new Date('2024-05-29 06:15:00')
-const hoje = Date.now()
-return Math.floor((data - hoje) / 1000)
-
+  return Math.floor((data - hoje) / 1000 )
 }
-contagemRegresssiva(dataEvento())
+
+
+contagemRegressiva(tempoRestante())
+
 
